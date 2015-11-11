65695c4add36f9838cffb9ce0d4f5d6e72c7d657
Kumseok Jung <jungkumseok@gmail.com>
2015-11-11 01:29:57 -0800

    Added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'jokes',
+  packages = ['jokes'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Experimental language written in python',
+  author = 'Kumseok Jung',
+  author_email = 'jungkumseok@gmail.com',
+  url = 'https://github.com/jungkumseok/jokes',
+  download_url = 'https://github.com/jungkumseok/jokes/archive/0.1.tar.gz',
+  keywords = ['testing', 'language'], 
+  classifiers = [],
+)
