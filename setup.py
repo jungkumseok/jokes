a93bfc85c4c326f6fbd6e250f6c40b55594bf119
Kumseok Jung <jungkumseok@gmail.com>
2016-01-18 13:53:59 -0800

    Added package_data

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 VERSION = '0.1.1'
 setup(
   name = 'jokes',
-  packages = ['jokes'], # this must be the same as the name above
+  packages = ['jokes',
+              'jokes.modules'],
+  package_data = {'jokes':['scripts/*']},
   version = VERSION,
   description = 'Experimental language written in python',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
-  license = 'LICENSE.txt',
+  license = 'LICENSE',
   url = 'https://github.com/jungkumseok/jokes',
   download_url = 'https://github.com/jungkumseok/jokes/archive/'+VERSION+'.tar.gz',
   keywords = ['testing', 'language'], 
