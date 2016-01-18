dda271596056826ef72cb5b2b2888e585c5df5cd
Kumseok Jung <jungkumseok@gmail.com>
2016-01-18 13:49:03 -0800

    Updated .gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+VERSION = '0.1.1'
 setup(
   name = 'jokes',
   packages = ['jokes'], # this must be the same as the name above
-  version = '0.1',
+  version = VERSION,
   description = 'Experimental language written in python',
   author = 'Kumseok Jung',
   author_email = 'jungkumseok@gmail.com',
+  license = 'LICENSE.txt',
   url = 'https://github.com/jungkumseok/jokes',
-  download_url = 'https://github.com/jungkumseok/jokes/archive/0.1.tar.gz',
+  download_url = 'https://github.com/jungkumseok/jokes/archive/'+VERSION+'.tar.gz',
   keywords = ['testing', 'language'], 
   classifiers = [],
 )
